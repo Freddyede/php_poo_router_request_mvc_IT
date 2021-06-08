30b7465e38e5bd65a7b32b5a19fe643dd72a173e
Eliot Demots <eliot@demots.fr>
2021-06-08 11:09:10 +0200

    add: TP1 Addo

--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+// PHP-FIG: PSR-4 autoloader
 spl_autoload_register(function ($classname) {
     // On change les \ en / et on rajoute .php
     $filename = __DIR__ . "/" . str_replace("\\", DIRECTORY_SEPARATOR, $classname) . ".php";
--- src/index.php
+++ src/index.php
@@ -1,3 +1,16 @@
 <?php
 
+use Animals\Sauvage\Elephant;
+use Animals\Giraf;
+
 require 'autoload.php'; // Pas toucher svp
+
+echo "• Elephant" . PHP_EOL;
+$elephant = new Elephant("Babar");
+$elephant->manger();
+$elephant->dormir();
+
+echo "• Giraf" . PHP_EOL;
+$giraf = new Giraf("Sophie");
+$giraf->manger();
+$giraf->dormir();
