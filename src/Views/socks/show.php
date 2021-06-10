07cd681b9c59106f21d24778baec6abd0c91fa5d
Patouillard <patouillardfranck3@gmail.com>
2021-06-10 23:35:32 +0200

    Edit title of show views

--- src/Views/socks/show.php
+++ src/Views/socks/show.php
@@ -21,7 +21,7 @@
     </head>
     <body>
         <?php require_once __DIR__ . '/../components/navbar.php' ?>
-        <h1 class="text-center mt-5 mb-5">Nom <?php foreach ($animalsView as $animal) : echo $animal->name; endforeach;?>  :</h1>
+        <h1 class="text-center mt-5 mb-5"><?php foreach ($animalsView as $animal) : echo $animal->name; endforeach;?>  :</h1>
         <div class="container">
             <table class="table text-center">
                 <thead>
