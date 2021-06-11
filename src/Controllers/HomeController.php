b7da76a14eb4f1dc7026797a985c3766210a4cb6
Patouillard <patouillardfranck3@gmail.com>
2021-06-11 16:57:40 +0200

    delete method delete from HomeController.php

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -10,7 +10,6 @@ use Core\View;
 final class HomeController extends AbstractController
 {
     public function index() {
-        $this->deleteController('socks',4);
         $url = $this->url;
         $title = $this->title;
         new View('home/index',compact("url","title"));
