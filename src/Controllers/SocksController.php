78b6a38d77fdaebce3d7c671330df1a713f48987
Mow <8723265-mow69@users.noreply.gitlab.com>
2021-06-13 18:50:42 +0200

    add view for create and update pages

--- src/Controllers/SocksController.php
+++ src/Controllers/SocksController.php
@@ -13,18 +13,49 @@ final class SocksController extends AbstractController
         $animals = $this->findAll(self::TABLE);
         $url = $this->url;
         $title = $this->title;
-        new View('socks/index', compact("animals","url","title"));
+        new View('socks/index', compact("animals", "url", "title"));
     }
     public function show($request, $id)
     {
         $url = $this->url;
         $title = $this->title;
         $animalsView = $this->find(self::TABLE, $id);
-        new View('socks/show', compact("animalsView","url","title"));
+        new View('socks/show', compact("animalsView", "url", "title"));
     }
 
     public function create($request)
     {
-        var_dump($request->getBody());
+        $url = $this->url;
+        $title = $this->title;
+        $animalsView = null;
+
+        new View('socks/create', compact("animalsView", "url", "title"));
+    }
+
+    public function persistInsert()
+    {
+        if (isset($_POST)) {
+            $this->insertController('socks', $_POST);
+        }
+        header('Location: /socks');
+    }
+
+    public function update($request,  $id)
+    {
+
+        $url = $this->url;
+        $title = $this->title;
+        $animalsView = $this->find(self::TABLE, $id);
+
+        new View('socks/update', compact("animalsView", "url", "title"));
+    }
+
+    public function persistUpdate($request, $id)
+    {
+
+        if (isset($_POST)) {
+            $this->updateController('socks', $id, $_POST);
+        }
+        header('Location: /socks');
     }
 }
